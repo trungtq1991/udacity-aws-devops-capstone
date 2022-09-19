a7927f09a306248325d68253efaefedb4bbabfde
trungtq1991 <trungtq1991@gmail.com>
2022-09-19 14:54:17 +0000

    Fix code app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-#import logging
 from flask import Flask
 
 app = Flask(__name__)
-#LOG = create_logger(app)
-#LOG.setLevel(logging.INFO)
 
 @app.route("/")
 def home():
@@ -11,5 +8,5 @@ def home():
     return html.format(format)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=80, debug=True)
 
