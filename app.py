892222c61ba383782a6c9ee84f94fadfca19d165
TrungTQ <trungtq1991@gmail.com>
2022-09-21 23:39:38 +0700

    Linting Pass

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     html = f"<h3>Hello World, my name is TrungTQ7</h3>"
-    return html.format(format)x
+    return html.format(format)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
